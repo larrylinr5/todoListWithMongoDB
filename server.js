1e0a390d10bbc3d2af50d0d3a99a3034229ad9d2
Larry <larrylinr5@gmail.com>
2022-04-10 16:21:46 +0800

    新增 => mongoDB連線功能 (載入 mongoose 套件、連線字串設定)

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const http = require("http");
+/** 載入 mongoose 套件 */
+const mongoose = require('mongoose');
+
+//#region 連接資料庫
+mongoose.connect('mongodb://localhost:27017/hotel')
+  .then(() => {
+    console.log('資料庫連線成功')
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+//#endregion
 
 const requestListener = (req, res) => {
   console.log('hello')
