558e971174d5d324e977e1fbfdffd00c164ff8af
Larry <larrylinr5@gmail.com>
2022-04-12 09:03:22 +0800

    調整 => 連線設定 (本地連線 > 遠端連線)

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const http = require("http");
 /** 載入 mongoose 套件 */
 const mongoose = require('mongoose');
+/** 載入 全域變數套件 */
+const dotenv = require('dotenv')
+
+// 全域變數套件設定
+dotenv.config({ path: "./config.env" })
 
 //#region 連接資料庫
-mongoose.connect('mongodb://localhost:27017/hotel')
+// 本地連線
+//mongoose.connect('mongodb://localhost:27017/hotel')
+
+// 遠端連線
+const connectString = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+)
+
+mongoose.connect(connectString)
   .then(() => {
     console.log('資料庫連線成功')
   })
@@ -17,4 +31,4 @@ const requestListener = (req, res) => {
 }
 
 const server = http.createServer(requestListener);
-server.listen(3005);
+server.listen(process.env.PORT);
